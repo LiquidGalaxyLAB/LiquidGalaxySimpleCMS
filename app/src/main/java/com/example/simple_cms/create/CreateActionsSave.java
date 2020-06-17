37d4b55531c752798dbe1bc39aa8e14b5af8a6bc
Diego <df.riveros11@uniandes.edu.co>
2020-06-17 15:09:44 -0500

    create skeleton of placemark, save ui

--- app/src/main/java/com/example/simple_cms/create/CreateActionsSave.java
+++
@@ -1,6 +0,0 @@
-package com.example.simple_cms.create;
-
-import android.app.Activity;
-
-public class CreateActionsSave extends Activity {
-}
---
+++ app/src/main/java/com/example/simple_cms/create/CreateStoryBoardActionPlaceMarkActivity.java
@@ -0,0 +1,19 @@
+package com.example.simple_cms.create;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.simple_cms.R;
+
+public class CreateStoryBoardActionPlaceMarkActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_storyboard_action_place_mark);
+
+    }
+}
